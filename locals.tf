5ebbd44787fc912095160ab1c5f51e85932df6ee
esenebenjamin <eseneehis@yahoo.com>
2022-12-11 21:21:50 -0800

    timestamp format update

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
     company = "jjtech"
     owner = "jjtech DevOps Team"
     team-email = "team-devops@jjtech.com"
-    time = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())    
+    time = formatdate("DD MMMM YYYY hh:mm ZZZ", timestamp())
   }
  
 }
